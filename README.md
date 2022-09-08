809fe8595594b6ab4c53bbdd70c0350856e5ae09
Jordan Laramie <jordanlaramie@gmail.com>
2022-09-08 14:15:17 -0400

    Added in a sample setup for indexed date based entity properties

--- README.md
+++ README.md
@@ -1,8 +1,48 @@
-# Atlassian Connect App using Express
+# ECOHELP-1144
 
-Congratulations!
-You've successfully created an Atlassian Connect App using the Express web application framework.
+## Install
 
-## What's next?
+1. Edit `config.json` with a correct `localBaseUrl` value.
 
-[Read the docs](https://bitbucket.org/atlassian/atlassian-connect-express/src/master/README.md).
+2. Run `./npm run watch` or `./npm build` and `./npm start`.
+
+3. Install the addon
+
+## Running / Testing
+
+1. Visit an issue in JIRA Cloud.
+
+2. Find the `ECOHELP-1144 Issue View` issue left panel.
+
+   _NOTE: This should display automatically but if it is not, clicking the three dots should present the panel as an option to show._
+
+   _NOTE: Panel must be displayed in order for the logic to run properly._
+
+3. The panel will perform an entity property lookup and do the following:
+
+   1. If the entity property doesn't exist, a new entity property will be created using the `/rest/api/3/issue/${issue.id}/properties/history_last_index_date`. It's value will then be displayed in this panel.
+
+   2. If the entity property already exists, the current value will display in the panel, as well as a button for updating it to the latest `DateTime`.
+
+4. Check JQL Queries
+
+   Included in the issue panel are two JQL calls that can be called, can be refreshed, and will display their output.
+
+   There are also a few queries that can be called that provide useful info for this issue.
+
+   - WORKS `/rest/api/3/search?jql=(last_index_date%20is%20not%20empty)`
+   - WORKS `/rest/api/3/search?jql=(last_index_date%20<%20NOW())`
+   - WORKS `/rest/api/3/search?jql=(last_index_date%20<%20"2022-09-08%2017%3A27")`
+   - WORKS `/rest/api/3/search?jql=(last_index_date%20<%20NOW())`
+   - DOES NOT WORK `/rest/api/3/search?jql=(last_index_date%20>%20created)`
+   - DOES NOT WORK `/rest/api/3/search?jql=(last_index_date%20>%20createdDate)`
+   - DOES NOT WORK `/rest/api/3/search?jql=(last_index_date%20>%updated)`
+   - DOES NOT WORK `/rest/api/3/search?jql=(last_index_date%20>%20updatedDate)`
+
+## Notes
+
+- According to https://developer.atlassian.com/cloud/jira/platform/jira-entity-properties/
+  ```
+  date: indexed as a date and allows for date range searching and ordering. The expected date format is [YYYY]-[MM]-[DD]. The expected date time format is [YYYY]-[MM]-[DD]T[hh]:[mm]:[ss] with an offset from UTC of: +/-[hh]:[mm] or Z for no offset. For reference, please see ISO_8601 standard.
+  ```
+  While entity properties can be stored as a date `YYYY-MM-DD` or a datetime `YYYY-MM-DDTHH:mm:ssZ` with no milliseconds the JIRA search api only seems searchable using various formats that do not contain `seconds`.
--- views/hello-world.jsx
+++ views/hello-world.jsx
@@ -1,34 +1,135 @@
-import SectionMessage from "@atlaskit/section-message";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import moment from "moment";
+
+function getEntityPropertyForIssue(issueIdOrKey) {
+  // eslint-disable-next-line no-undef
+  return window.AP.request(
+    `/rest/api/2/issue/${issueIdOrKey}/properties/history_last_index_date`,
+    {}
+  );
+}
+
+function setEntityPropertyForIssue(issueIdOrKey, date = new Date()) {
+  // eslint-disable-next-line no-undef
+  return window.AP.request(
+    `/rest/api/2/issue/${issueIdOrKey}/properties/history_last_index_date`,
+    {
+      type: "PUT",
+      contentType: "application/json",
+      data: JSON.stringify({
+        // last_updated_date: date.toISOString(),
+        last_updated_date: moment.utc(date).format("YYYY-MM-DDTHH:mm:ss") + "Z",
+      }),
+    }
+  );
+}
+
+function getJQLSearch(jql) {
+  // eslint-disable-next-line no-undef
+  return window.AP.request(`/rest/api/3/search?jql=${jql}`, {});
+}
 
 export default function HelloWorld() {
-  const [excitementLevel, setExcitementLevel] = React.useState(0);
+  const [context, setContext] = useState("");
+  const [entityPropertyData, setEntityPropertyData] = useState({});
+  const [jqlSearchWorking, setJqlSearchWorking] = useState({});
+  const [jqlSearchBroken, setJqlSearchBroken] = useState({});
+
+  useEffect(() => {
+    // eslint-disable-next-line no-undef
+    window.AP.context.getContext().then((context) => setContext(context));
+  }, []);
+
+  useEffect(() => {
+    if (context) {
+      getEntityPropertyForIssue(context.jira.issue.key).then(
+        (entityProperty) => setEntityPropertyData(entityProperty),
+        () => {
+          setEntityPropertyForIssue(context.jira.issue.key).then(() =>
+            getEntityPropertyForIssue(context.jira.issue.key).then(
+              (entityProperty) => {
+                setEntityPropertyData(entityProperty);
+              }
+            )
+          );
+        }
+      );
+    }
+  }, [context]);
+
   return (
-    <SectionMessage
-      title={`Hello, world${
-        excitementLevel ? new Array(excitementLevel).fill("!").join("") : "."
-      }`}
-      actions={[
-        {
-          key: "1",
-          href: "https://atlassian.design/components/",
-          text: "Browse more components to add to your app",
-        },
-        {
-          key: "2",
-          onClick: () => setExcitementLevel(excitementLevel + 1),
-          text: "Get excited!",
-        },
-      ]}
-    >
-      <p>
-        Congratulations! You have successfully created an Atlassian Connect app
-        using the{" "}
-        <a href={"https://bitbucket.org/atlassian/atlassian-connect-express"}>
-          atlassian-connect-express
-        </a>{" "}
-        client library.
-      </p>
-    </SectionMessage>
+    <div>
+      <button
+        onClick={() => {
+          // eslint-disable-next-line no-debugger
+          debugger;
+        }}
+      >
+        Debugger
+      </button>
+      {entityPropertyData.body && (
+        <button
+          onClick={() => {
+            setEntityPropertyForIssue(context.jira.issue.key).then(() =>
+              getEntityPropertyForIssue(context.jira.issue.key).then(
+                (entityProperty) => {
+                  setEntityPropertyData(entityProperty);
+                }
+              )
+            );
+          }}
+        >
+          Update Entity Property
+        </button>
+      )}
+      <br />
+      <br />
+      <label>
+        Context: <br />
+        <code>{JSON.stringify(context)}</code>
+      </label>
+      <br />
+      <br />
+      <label>
+        Entity Property: <br />
+        <code>{JSON.stringify(entityPropertyData)}</code>
+      </label>
+      <br />
+      <br />
+      <label>
+        JQL Search via Entity Property (Working):{" "}
+        <button
+          onClick={() => {
+            getJQLSearch("(last_index_date%20is%20not%20empty)").then(
+              (result) => {
+                setJqlSearchWorking(result.body);
+              }
+            );
+          }}
+        >
+          Search
+        </button>
+        <pre>/rest/api/3/search?jql=(last_index_date%20is%20not%20empty)</pre>{" "}
+        <br />
+        <code>{JSON.stringify(jqlSearchWorking)}</code>
+      </label>
+      <br />
+      <br />
+      <label>
+        JQL Search via Entity Property (Broken):{" "}
+        <button
+          onClick={() => {
+            getJQLSearch("(last_index_date%20>%20created)").then((result) => {
+              setJqlSearchBroken(result.body);
+            });
+          }}
+        >
+          Search
+        </button>
+        <pre>/rest/api/3/search?jql=(last_index_date%20&gt;%20created)</pre>{" "}
+        <br />
+        <code>{JSON.stringify(jqlSearchBroken)}</code>
+      </label>
+    </div>
   );
 }
